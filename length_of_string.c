221566a5a1b19760c3bcbf7019888d10d0ebbac6
Vishwajeet-singh28 <vishwajeetsinghrajput75@gmail.com>
2026-03-25 23:09:05 +0530

    Add program to calculate length of a string

---
+++ length_of_string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char s[100];
+    printf("Enter the string: ");
+    scanf("%s", s);
+    int l = 0;
+    for(l = 0; s[l] != '\0'; l++) {}
+    printf("Length of the string: %d\n", l);
+    return 0;
+}
