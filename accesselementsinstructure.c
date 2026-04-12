07ce46d9edab6331b9665987bc8b91b5da3d1570
VISHWAJEET SINGH <vishwajeetsinghrajput75@gmail.com>
2026-04-12 13:25:38 +0530

    Add structure example with element access in C

---
+++ accesselementsinstructure.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+struct mystructure{
+    int mynum;
+    int myletter;
+};
+
+int main(){
+    struct mystructure s1;
+    struct mystructure s2;
+    
+    s1.mynum=12;
+    s1.myletter='V';
+    
+    s2.mynum=24;
+    s2.myletter='J';
+    
+    printf("my number= %d\n", s1.mynum);
+    printf("my letter=%c\n", s1.myletter);
+    
+    printf("my number=%d\n",s2.mynum);
+    printf("my letter=%c\n",s2.myletter);
+    
+    return 0;
+}
